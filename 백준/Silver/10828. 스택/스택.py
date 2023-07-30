f1d13a041eb66aa333e9d89db60d04cd3bcfff2e
jgh05168 <97810918+jgh05168@users.noreply.github.com>
2023-07-30 14:42:49 +0900

    update_memo

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -18,12 +18,12 @@ for test_case in range(0, N):
     elif order[0] == 'size':
         print(len(stack))
     elif order[0] == 'empty':
-        if len(stack) == 0:  # 수정: 빈 스택인지 확인
+        if len(stack) == 0:  # 스택이 비었는지 아닌지 유무를 확인하기 위해서는 stack == [] 대신 len(stack)을 사용하자.
             print(1)
         else:
             print(0)
     elif order[0] == 'top':
-        if len(stack) == 0:  # 수정: 빈 스택인지 확인
+        if len(stack) == 0:
             print(-1)
         else:
             print(stack[-1])
