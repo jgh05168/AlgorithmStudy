d1e5e7823e0e371232407189a3c5ee607e6ede70
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-02 16:08:15 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,11 +1,13 @@
 
 def solution(numbers):
-    answer = ''
 
     numbers = list(map(str, numbers))
     numbers.sort(key = lambda x : x * 4 ,reverse=True)       # sort / sorted 함수 사용 시 key = lambda x : 조건 을 사용하여 쉽게 정렬할 수 있다.
 
-    for i in numbers:
-        answer += i
 
-    return str(int(answer))
+    return str(int(''.join(numbers)))
+
+print(solution([0, 0, 0, 0]))
+
+
+
