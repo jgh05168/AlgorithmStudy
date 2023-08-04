ad8670ef20a85a51b88e3cda3a07db257cf37fcc
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-04 15:18:36 +0900

    [D2] Title: 간단한 소인수분해, Time: 148 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,18 +1,18 @@
 T = int(input())
 
 for tc in range(1, T + 1):
-    N = int(input())
-    prime = [[2, 0], [3, 0], [5, 0], [7, 0], [11, 0]]
-    prime_cnt = []
-    while N != 1:
-        for div in prime:
-            if N % div[0] == 0:
-                div[1] += 1
-                N /= div[0]
-                break
+    N = { '2' : 0, '3' : 0, '5' : 0, '7' : 0, '11' : 0}
+    val = int(input())
 
+    while val > 1:
+        for i in N.keys():
+            if val % int(i) == 0:
+                N[i] += 1
+                val = val // int(i)
 
-    print(f'#{tc}', end=' ')
-    for cnt in prime:
-        print(cnt[1], end=' ')
-    print()
+    cnt = []
+    for i in N.values():
+        cnt.append(i)
+
+    print(f'#{tc} ', end='')
+    print(*cnt)
