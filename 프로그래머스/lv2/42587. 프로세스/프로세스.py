ca131df7549e9ad9e5dd1a18df3986267cf3237f
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-09 12:07:02 +0900

    [level 2] Title: 프로세스, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -4,12 +4,13 @@ def solution(priorities, location):
 
     while 1:
         ans = queue.pop(0)
+        # any : iterable 한 반복문 중 단 하나라도 참인 값이 존재한다면, True를 반환
         if any(ans[1] < q_val[1] for q_val in queue):
             queue.append(ans)
         else:
             cnt += 1
-            if ans[0] == location:
-                return cnt
+            if ans[0] == location:      # location과 ans의 인덱스 넘버가 같은지 확인
+                return cnt              # 같으면 횟수를 바로 return
 
 
     return cnt
