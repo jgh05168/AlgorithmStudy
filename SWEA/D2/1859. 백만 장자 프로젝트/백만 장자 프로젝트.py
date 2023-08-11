8d82ac2afbd4c34f80d2e83ad83a6cee82f6647f
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-11 10:31:29 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,200 ms, Memory: 242,256 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -6,9 +6,10 @@ for test_case in range(1, T+1):
     weight_per_day = list(map(int, input().split()))
 
     margine = 0
-    selling_price = 0
+    selling_price = 0       # 주어진 배열 중 가장 비싼 판매가(이 가격에 파는게 가장 good)
 
     for price in weight_per_day[::-1]:
+        # print(price)
         if price >= selling_price:
             selling_price = price
         else:
