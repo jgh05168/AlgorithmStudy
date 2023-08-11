00891ca779c8172773e24dd08a74e04064cf5218
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-11 14:10:56 +0900

    [D3] Title: 농작물 수확하기, Time: 237 ms, Memory: 63,308 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,5 +1,5 @@
 T = int(input())
-
+#      5시 7시 11시 1시     방향
 drow = [1, 1, -1, -1]
 dcol = [1, -1, -1, 1]
 
@@ -8,14 +8,14 @@ for tc in range(1, T + 1):
 
     harvest = [list(map(int, input())) for _ in range(N)]
 
-    get_range = N // 2
-    margin = harvest[get_range][get_range]
+    get_range = N // 2          # 중앙점 위치를 알기 위한 계산
+    margin = harvest[get_range][get_range]  # 농작물의 가장 중앙값을 초기값으로
 
-
+    # 안쪽부터 바깥쪽까지 대각선 방향으로 훓을 것이다.
     for k in range(1, get_range + 1):
-        row, col = get_range - k, get_range
-        for d in range(4):
-            for i in range(k):
+        row, col = get_range - k, get_range     # 방향벡터를 다 돌았으면 row - 1을 해주어 한칸 위로 올린다.
+        for d in range(4):             # 방향 벡터 설정
+            for i in range(k):         # 얼만큼 대각선 방향으로 이동할 것인지 k번으로 설정
                 nrow = row + drow[d]
                 ncol = col + dcol[d]
                 # print(nrow, ncol)
