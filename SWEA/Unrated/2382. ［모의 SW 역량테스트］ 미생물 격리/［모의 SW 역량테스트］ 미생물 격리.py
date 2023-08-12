41fc6d7dca9a1b973021412c184e4f77e8f775ef
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-12 19:46:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 807 ms, Memory: 76,532 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.py
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.py
@@ -1,29 +1,35 @@
+# List와 Dictionary로 문제 해결
+# List에 [[y축 좌표, x축 좌표], 미생물 수, 방향] 식으로 저장
+# transform_list 함수를 사용하여 미생물들이 한곳에 모이는 경우(x, y 좌표가 같은 경우) 미생물의 개수에 따라 방향을 정해주고, 값을 더해주어 반환
 T = int(input())
 
-def transform_list(input_list):
-    result_dict = {}
-    same_dict = {}
-    # print(input_list)
-    for bug in input_list:
-        loc = tuple(bug[0])
-        if loc not in result_dict.keys():
-            result_dict.update({loc: (bug[1], bug[2])})
-        else:
-            if loc not in same_dict.keys():
-                same_dict.update({loc : [result_dict[loc]]})
-            same_dict[loc].append((bug[1], bug[2]))
 
-    for key, value in same_dict.items():
-        max_bug = 0
-        value.sort(key=lambda x: x[0], reverse=True)
-        max_dir = value[0][1]
-        for val in value:
-            max_bug += val[0]
-        result_dict.update({key: (max_bug, max_dir)})
+def transform_list(input_list):
+    result_dict = {}        # (y, x) : (미생물 수, 방향) 꼴의 dictionary 생성
+    same_dict = {}          # (y, x)가 같은 값들에 대해 dictionary 생성
+                            # (y, x) : [(미생물 수1, 방향1), (미생물 수2, 방향2) ...] 꼴로 저장하여 하나의 좌표에 대해 여러 개의 정보를 비교
+
+    for bug in input_list:      # input_list의 각 좌표([[y, x], 미생물 수, 방향])을 순차적으로 탐색
+        loc = tuple(bug[0])     # 불러온 좌표 정보를 튜플로 생성
+        if loc not in result_dict.keys():   # result_dict 의 key에 좌표 정보가 없다면
+            result_dict.update({loc: (bug[1], bug[2])})     # result_dict에 좌표 정보를 key로, value에 ( 미생물 수, 방향) 모양으로 저장
+        else:               # key에 좌표 정보가 있다면 ==> 미생물의 위치가 겹치는 부분이 존재한다!
+            if loc not in same_dict.keys():         # 만약 없다는 것을 확인했는데, same_dict에 한번도 저장된 적이 없다면 ?
+                same_dict.update({loc : [result_dict[loc]]})        # result_dict에 저장된 초기 키, 값 정보를 업데이트
+            same_dict[loc].append((bug[1], bug[2]))         # 이후 현재 겹치는 부분의 새로운 정보를 저장
+
+    # same_dict에 저장된 값들을 하나씩 비교하여 미생물의 최고 개수와 최고 개수의 방향을 찾자
+    for key, value in same_dict.items():        # same_dict의 key와 value를 items()를 이용하여 불러옴
+        max_bug = 0            # 미생물의 총 합을 구하기 위해 초기화
+        value.sort(key=lambda x: x[0], reverse=True)        # 좌표가 같은 것들을 미생물 수를 기준으로 내림차순 정렬(lambda 숙지)
+        max_dir = value[0][1]           # 정렬된 가장 큰 미생물 수의 좌표를 max_dir로 설정
+        for val in value:               # 값에 들어있는 각각의 정보에 대해
+            max_bug += val[0]           # 미생물 총 개수를 위해 더해준다.
+        result_dict.update({key: (max_bug, max_dir)})       # result_dict에 새로운 정보를 덮어씌워 업데이트 해준다.
 
     # print(result_dict)
     # print()
-    result_list = [[list(key), value[0], value[1]] for key, value in result_dict.items()]
+    result_list = [[list(key), value[0], value[1]] for key, value in result_dict.items()]   # input_list와 같은 꼴로 변환 후 반환
     return result_list
 
 
