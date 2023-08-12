f76b64c36e39d12a15c1b8c5f14e9b061eb2fd9b
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-12 20:23:46 +0900

    [D2] Title: 풍선팡2, Time: 153 ms, Memory: 59,964 KB -BaekjoonHub

--- SWEA/D2/16268. 풍선팡2/풍선팡2.py
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.py
@@ -1,25 +1,25 @@
 T = int(input())
 
-for tc in range(1, T+1):
+#      우  하  좌  상
+drow = [0, 1, 0, -1]
+dcol = [1, 0, -1, 0]
 
-    N, M = list(map(int, input().split()))
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
 
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    flowers = [list(map(int, input().split())) for _ in range(N)]
 
-    max_value = 0
-
-    drow = [0, 1, 0, -1]
-    dcol = [1, 0, -1, 0]
-
+    max_pang = 0
     for row in range(N):
         for col in range(M):
-            s = arr[row][col]
-            for k in range(len(drow)):
-                nrow = row + drow[k]
-                ncol = col + dcol[k]
-                if 0 <= nrow and N > nrow and 0 <= ncol and M > ncol:
-                    s += arr[nrow][ncol]
-            if max_value < s:
-                max_value = s
+            pang = flowers[row][col]
+            for d in range(len(drow)):
+                nrow = row + drow[d]
+                ncol = col + dcol[d]
+                if 0 <= nrow < N and 0 <= ncol < M:
+                    pang += flowers[nrow][ncol]
 
-    print(f'#{tc} {max_value}')
+            if max_pang < pang:
+                max_pang = pang
+
+    print(f'#{tc} {max_pang}')
