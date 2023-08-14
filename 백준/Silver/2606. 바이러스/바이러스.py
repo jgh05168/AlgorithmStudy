0b182be5975b4dbb9a1086bca6a49e3e281a57aa
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-14 23:41:55 +0900

    [Silver III] Title: 바이러스, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,6 +1,7 @@
 V = int(input())
 E = int(input())
 
+# dfs(재귀)
 def dfs(v):
     if visited[v] == True:
         return
@@ -12,30 +13,31 @@ def dfs(v):
             if visited[w] == False:
                 dfs(w)
 
-graphs = {}
-visited = [False] * (V + 1)
+graphs = {}     # 인접리스트 생성을 위한 딕셔너리
+visited = [False] * (V + 1)     # 방문한 노드인지 확인하는 리스트
 for _ in range(E):
-    v, w = map(int, input().split())
+    v, w = map(int, input().split())        # 현재노드, 인접노드 input
+    # 무방향 그래프이기 때문에 양 쪽 모두에 대한 간선을 연결해 주어야 한다.
+    # 정방향
     if v in graphs.keys():
         graphs[v].append(w)
     else:
-        graphs.update({v:[w]})
+        graphs.update({v: [w]})
+    # 역방향
     if w in graphs.keys():
         graphs[w].append(v)
     else:
-        graphs.update({w:[v]})
+        graphs.update({w: [v]})
 
-
-
-for key in graphs.keys():
+for key in graphs.keys():       # 1번 노드부터 탐색 시작
     if key == 1:
         dfs(key)
 
-cnt = - 1
+cnt = - 1       # 시작 노드 1을 제외하기 위해 초기값을 -1로 설정
 for val in visited:
-    if val == True:
+    if val == True:         # 1에서 시작되는 노드에 방문했을 시
         cnt += 1
-if visited[1] == False:
-    cnt = 0
+if visited[1] == False:     # 만약 시작노드 1이 없다면?
+    cnt = 0                 # 바이러스에 감염된 pc가 없으므로 0으로 값 재설정
 # print(visited)
 print(cnt)
