f38c6c1f134ee6f7ec584068b0ee78b3fd512274
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-17 21:00:31 +0900

    [Silver III] Title: 근손실, Time: 96 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.py
+++ 백준/Silver/18429. 근손실/근손실.py
@@ -1,27 +1,29 @@
 def backtracking(i, N, weight):
     global cnt
 
+    # 만약 case를 다 돌았다면 = 경우의 수가 존재한다
     if i == N:
-        cnt += 1
+        cnt += 1        # 카운트
         return
 
+    # 3대 500보다 중량이 낮아진 경우, 백트래킹
     if weight < 500:
         return
 
     else:
-        for day in range(N):
-            if selected[day] == False:
-                selected[day] = True
-                weight = weight - K + A[day]
-                backtracking(i + 1, N, weight)
-                weight = weight + K - A[day]
-                selected[day] = False
+        for day in range(N):        # 모든 날에 대해
+            if selected[day] == False:      # 만약 사용하지 않은 키트라면
+                selected[day] = True        # 사용함으로 변경
+                weight = weight - K + A[day]    # 운동 후 중량 업데이트
+                backtracking(i + 1, N, weight)  # 다음 날 확인
+                weight = weight + K - A[day]    # 이전에 사용한 조건을 back시켜줌
+                selected[day] = False           # 사용하지 않음 으로 업데이트
 
 cnt = 0
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
-weight = 500
-selected = [False] * N
+weight = 500        # 대학원생 중량
+selected = [False] * N  # 그 날 사용한 키트에 대해 표시
 
 backtracking(0, N, weight)
 
