a016ef6c59fca3054c43b65234cc7a172e1e960c
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-18 16:05:44 +0900

    Update 적록색약.py
    주석 추가

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,6 +1,9 @@
+
+# 방향벡터 생성(대각선의 경우 그림을 구분하는 데 사용되는 정보)
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
+# 일반인의 bfs -- 모든 색에 대해 구분이 가능하다.
 def normal(srow, scol, visited, color):
     queue = []
     queue.append((srow, scol))
@@ -17,6 +20,7 @@ def normal(srow, scol, visited, color):
     return 1
 
 
+# 적녹색약의 bfs -- 파랑색과 그 외 나머지 색에 대한 case를 구분지어야 한다.
 def problem(srow, scol, visited, color):
     queue = []
     queue.append((srow, scol))
@@ -40,8 +44,8 @@ def problem(srow, scol, visited, color):
 N = int(input())
 
 picture = [list(input()) for _ in range(N)]
-normal_visited = [[False] * N for _ in range(N)]
-problem_visited = [[False] * N for _ in range(N)]
+normal_visited = [[False] * N for _ in range(N)]        # 정상인 기준 방문배열
+problem_visited = [[False] * N for _ in range(N)]       # 적녹색약인 기준 방문배열
 
 normal_look = 0
 problem_look = 0
