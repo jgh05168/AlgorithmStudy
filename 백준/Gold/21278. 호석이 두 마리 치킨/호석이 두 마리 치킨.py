aba7b1313517222321242467bf2e9d575a00813c
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-08-29 16:44:26 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 3788 ms, Memory: 31256 KB

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
@@ -1,4 +1,5 @@
 def bfs(city):
+    # 만약 현재 노드가 치킨집이면 거리 0 반환
     if city == chickens[0] or city == chickens[1]:
         return 0
     else:
@@ -9,9 +10,11 @@ def bfs(city):
             v = queue.pop(0)
             for w in graph[v]:
                 if not visited[w]:
+                    # 만약 인접한 도시가 치킨집이라면
                     if w == chickens[0] or w == chickens[1]:
-                        return visited[v]
-                    else:
+                        return visited[v]   # 현재 거리만큼 반환
+                        # 어떻게든 탐색이 될것이기 떄문에 반환이 안될 리는 없다.
+                    else:   # 아니라면 일반 bfs 수행
                         queue.append(w)
                         visited[w] = visited[v] + 1
 
@@ -27,12 +30,16 @@ for _ in range(M):
 
 
 dist = []
+# 가능한 치킨집 모두 탐색(순열처럼)
 for i in range(1, N):
     for j in range(i + 1, N + 1):
         chickens = (i, j)
         total_dist = 0
+        # 도시를 하나씩 탐색하며 치킨집과의 거리 bfs로 계산
         for city in range(1, N + 1):
-            total_dist += 2 * bfs(city)
+            total_dist += 2 * bfs(city)     # 왕복거리이기 때문에 *2
+
+        # 현재 좌표, 모든 도시에서 치킨집까지의 왕복 거리를 리스트에 저장
         dist.append([i, j, total_dist])
 
 dist.sort(key=lambda x: x[2])
