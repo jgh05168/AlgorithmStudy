f3224785128e43cfd91c2fb1c8ef96f2aa9e6fa9
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-09-26 17:24:10 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 1088 ms, Memory: 51540 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -0,0 +1,46 @@
+'''
+각 지점에 대해서 bfs를 돌려보자
+
+dfs + dp
+- 출발지부터 몇 칸 갔는지를 세어본다
+- 만약 이미 방문했던 지점이라면 반환
+- 아닌경우 max를 사용하여 순차적으로 dp 업데이트
+
+정답 : 판다가 랜덤한 위치에서 최대한 많이 이동할 수 있는 칸의 수의 최댓값
+'''
+
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
+
+
+def dfs(row, col):
+    if memo[row][col]:
+        return memo[row][col]
+
+    memo[row][col] = 1
+    for d in range(len(dr)):
+        nrow = row + dr[d]
+        ncol = col + dc[d]
+        if 0 <= nrow < n and 0 <= ncol < n and forest[row][col] < forest[nrow][ncol]:
+            memo[row][col] = max(memo[row][col], dfs(nrow, ncol) + 1)
+    return memo[row][col]
+
+
+n = int(input())
+forest = [list(map(int, input().split())) for _ in range(n)]
+memo = [[0] * n for _ in range(n)]
+
+result = 0
+for i in range(n):
+    for j in range(n):
+        if not memo[i][j]:
+            result = max(result, dfs(i, j))
+
+
+print(max(map(max, memo)))
