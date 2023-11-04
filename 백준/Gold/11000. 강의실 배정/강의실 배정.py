0169f585e4d256e0417d493400eba1cabb29bce5
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-11-04 22:06:58 +0900

    [Gold V] Title: 강의실 배정, Time: 624 ms, Memory: 61160 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -3,6 +3,9 @@
 
 1. 우선순위 큐에 (강의 시간, start, end) 형식으로 삽입
 2. 하나씩 꺼내보며 강의를 배정한다.
+
+- 시간표에 저장 시 (end, start) 순으로 우선순위 큐에 삽입할 것
+    -> 끝나는 시간이 빠른 것을 기준으로 정렬해두기
 '''
 
 import sys, heapq
@@ -19,7 +22,7 @@ while pq:
     start, end = heapq.heappop(pq)
     # 만약 시간표가 비었거나 가장 작은 시간표의 end보다 start가 더 빠르다면 그냥 새로운 강의실 지정
     if not timetable or timetable[0][0] > start:
-        heapq.heappush(timetable, (end, start))
+        heapq.heappush(timetable, (end, start))     # 끝나는 시간을 먼저 우선순위큐에 삽입
     else:       # 아니라면 현재 강의실은 이미 처리했으니 빼준 뒤 새로운 강의실로 배정
         heapq.heappop(timetable)
         heapq.heappush(timetable, (end, start))
