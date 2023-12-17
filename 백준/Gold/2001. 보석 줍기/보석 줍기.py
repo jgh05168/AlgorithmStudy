abc473682fdf335c7facb65798c0f99724f44bfe
전규훈 <97810918+jgh05168@users.noreply.github.com>
2023-12-17 17:03:20 +0900

    [Gold I] Title: 보석 줍기, Time: 780 ms, Memory: 240760 KB -BaekjoonHub

--- 백준/Gold/2001. 보석 줍기/보석 줍기.py
+++ 백준/Gold/2001. 보석 줍기/보석 줍기.py
@@ -14,6 +14,7 @@ def bfs(su):
     global max_jewel
 
     queue = deque()
+    # 현재 노드, 먹은 노드 수, 보석을 먹은 인덱스, 어떤 보석을 먹었는지 4가지 정보를 갖는다.
     queue.append((su, 0, 0, []))
     visited[0][su] = 1
 
@@ -29,10 +30,12 @@ def bfs(su):
                         max_jewel = max(max_jewel, cur_jewel)
                 # 보석을 줍는 경우와 줍지 않는 경우를 나눠야 한다.
                 else:
+                    # 무조건 안줍고 지나가는 경우가 존재
                     queue.append((v, cur_jewel, v_idx, get_jewel))
                     visited[v_idx][v] = 1
+                    # 보석이 존재한다면
                     if treasure_check[v]:
-                        if v in get_jewel:
+                        if v in get_jewel:      # 이미 먹은 보석인지 체크
                             continue
                         if cur_jewel <= w:
                             queue.append((v, cur_jewel + 1, v_idx + (1 << treasure_idx[v]), get_jewel + [v]))
