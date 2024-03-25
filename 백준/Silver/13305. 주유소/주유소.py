6633dec1138baedc13c577ac7110296d48f8b036
전규훈 <97810918+jgh05168@users.noreply.github.com>
2024-03-25 22:02:23 +0900

    [Silver III] Title: 주유소, Time: 48 ms, Memory: 31252 KB, Score: 41 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,24 @@
+'''
+도시들은 일직선 상에 있음
+
+기름통 : 무제한
+1km에 1L
+각 도시에는 단 하나의 주유소만 존재. 주유소의 리터당 가격은 다를 수 있음
+최소비용찾기
+
+풀이:
+1. 기름값 0원이라 하고 시작
+2. 도시를 하나씩 이동할수록 (기름값이 가장 작은 곳 * 도로의 길이)를 더해준다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+roads = [0] + list(map(int, input().split()))
+cities = list(map(int, input().split()))
+
+min_v = 0
+for i in range(1, n):
+    min_v += min(cities[:i]) * roads[i]
+print(min_v)
