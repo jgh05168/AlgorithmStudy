24858e88a2d551d2dda19d046af0ea7238e6900b
전규훈 <97810918+jgh05168@users.noreply.github.com>
2024-04-16 22:58:39 +0900

    [Gold V] Title: 행복 유치원, Time: 256 ms, Memory: 66600 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.py
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -1,35 +1,21 @@
 '''
-일렬로 줄 세우고 K개의 조로 나누려고 한다.
-
-- 정렬
-- 그리디 & 백트래킹
-
+n명의 원생들을 키 순서대로 세운다 -> 정렬
+k개의 조로 나누고자 한다 : 일단 인접한 애들의 값을 구해줘야 댐
+구한 다음, 인접한 값 위치를 오름차순으로 정렬한다.
+그리고, k개만큼만 더해주고 나머지는 날려
 '''
 
-import sys, heapq
+import sys
 input = sys.stdin.readline
 
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
 
-N, K = map(int, input().split())
-children = list(map(int, input().split()))
-children.sort()
+arr.sort()
 
-lst = []
-for i in range(N - 1):
-    heapq.heappush(lst, (-(children[i + 1] - children[i]), i, i + 1))
+adj_values = []
+for adj_idx in range(1, n):
+    adj_values.append(arr[adj_idx] - arr[adj_idx - 1])
 
-idx_list = []
-for i in range(K - 1):
-    val, s, e = heapq.heappop(lst)
-    idx_list.append(e)
-
-idx_list.sort()
-
-ans = 0
-start = 0
-for i in idx_list:
-    ans += children[i - 1] - children[start]
-    start = i
-ans += children[-1] - children[start]
-
-print(ans)
+adj_values.sort()
+print(sum(adj_values[:n - k]))
