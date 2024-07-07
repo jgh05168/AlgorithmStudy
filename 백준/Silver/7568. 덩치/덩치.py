003ba986c0627906e7967f06cfcd6270466fae4e
전규훈 <97810918+jgh05168@users.noreply.github.com>
2024-07-07 15:52:21 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,25 @@
+'''
+n이 50까지이므로 빡구현 드가자 ㅋ
+'''
+
+import sys, heapq
+input = sys.stdin.readline
+
+n = int(input())
+
+data = []       # 입력받은 정보를 저장할 리스트 data
+ans = []        # 등수정보를 저장할 리스트 ans
+for i in range(n):
+    a, b = map(int, input().split())
+    data.append((a, b))  # 몸무계와 키를 묶어서 append 해줌
+
+for i in range(n):
+    count = 0
+    for j in range(n):
+        # 몸무게와 키 모두 자신보다 큰 사람의 수를 센다
+        if data[i][0] < data[j][0] and data[i][1] < data[j][1]:
+            count += 1
+    ans.append(count + 1)   # 덩치 등수는 자신보다 몸무계 키 모두 큰 사람의 수 + 1 이므로 count + 1을 ans에 append한다.
+
+for d in ans:
+    print(d, end=" ")
