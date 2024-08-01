c3e80ac721fa8f4eedbe0c9ad2d1ee48613e88af
전규훈 <97810918+jgh05168@users.noreply.github.com>
2024-08-01 12:16:59 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,25 +1,14 @@
-'''
-퇴사
-
-날짜를 더해가면서, 초과하지 않는 경우, 값 업데이트하기
-
-'''
-
-import sys
-input = sys.stdin.readline
 
 n = int(input())
-schedule = []
+arr = []
 for _ in range(n):
-    schedule.append(tuple(map(int, input().split())))
-
+    arr.append(tuple(map(int, input().split())))
 dp = [0] * (n + 1)
-for i in range(n):
-    # 일정 안에 상담을 진행할 수 있어야 함.
-    # 업데이트 범위는 (일정 + 상담에 필요한 날짜) 부터 퇴사날까지 
-    for j in range(i + schedule[i][0], n + 1):
-        # 이후에는 가치만 놓고 비교하기
-        if dp[j] < dp[i] + schedule[i][1]:
-            dp[j] = dp[i] + schedule[i][1]
 
-print(dp[-1])
+for i in range(n):
+    dp[i] = max(dp[i], dp[i - 1])
+    day, val = arr[i]
+    if i + day <= n:
+        dp[i + day] = max(dp[i + day], dp[i] + val)
+
+print(max(dp))
