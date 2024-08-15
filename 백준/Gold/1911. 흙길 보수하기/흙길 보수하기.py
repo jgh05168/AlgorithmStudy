eeff3c2afa260e0641eb8866510d4538d6e9c93b
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-08-15 21:44:19 +0900

    [Gold V] Title: 흙길 보수하기, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -15,16 +15,36 @@ input = sys.stdin.readline
 
 n, l = map(int, input().split())
 water_list = [tuple(map(int, input().split())) for _ in range(n)]
-water_list.sort(key=lambda x: x[1])
 
-idx = water_list[-1][1]
+
 ans = 0
+#### 첫번째 방법(시간 오래 걸림)
+# water_list.sort(key=lambda x: x[0])
+# idx = water_list[-1][1]
+# while water_list:
+#     start, end = water_list.pop()
+#     if idx > end:
+#         idx = end
+#     while idx > start:
+#         idx -= l
+#         ans += 1
+
+#### 두번째 방법 : 두번째 while 문을 수학적으로 풀이
+water_list.sort(key=lambda x: x[0], reverse=True)
+idx = water_list[-1][0]
 while water_list:
     start, end = water_list.pop()
-    if idx > end:
+    if idx < start:
+        idx = start
+    # 거리 계산
+    diff = end - idx
+    # 다 막혀 떨어질 때 vs 안막힐 때 차이 비교
+    if not diff % l:
+        ans += diff // l
         idx = end
-    while idx > start:
-        idx -= l
-        ans += 1
+    else:
+        ans += diff // l + 1    # 무조건 막아야하므로 안막힌 부분을 덮기 위한 + 1
+        # 다 덮었다는 가정 하에 시작 위치 업데이트
+        idx = end + (l - diff % l)
 
 print(ans)
