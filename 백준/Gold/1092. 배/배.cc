f6392d89b32f08bccbbee0889802320e501ec3d5
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-08-22 19:34:34 +0900

    [Gold V] Title: 배, Time: 716 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.cc
@@ -0,0 +1,72 @@
+/*
+각 크레인은 무게 제한이 있다.  -> 무게 제한보다 무거운 박스는 크레인으로 움직일 수 없다.
+	이런 경우 -1 출력
+
+풀이 :
+50 x 10000
+- max 크레인 무게랑 비교한 뒤 만약 하나라도 무거우면 -1 출력 후 종료
+1. 우선 정렬 진행(내림차순)
+2. 무거운 순서로 하나씩 집어넣는다.
+	크레인에 하나씩 할당하거나, 다 넣는 경우까지 반복
+------ 시간초과 ------
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> crains;
+vector<int> boxes;
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+	cin >> n;
+	int tmp;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		crains.push_back(tmp);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> tmp;
+		boxes.push_back(tmp);
+	}
+
+	// 크레인, 컨테이너 정렬
+	sort(crains.begin(), crains.end(), greater<int>());
+	sort(boxes.begin(), boxes.end(), greater<int>());
+
+	int ans = 0;
+	int box_idx = 0;
+	int box_move = 0;
+
+	// 더 무거운 짐이 있는지 확인
+	if (crains[0] < boxes[0]) {
+		cout << -1 << '\n';
+	}
+	else {
+		while (!boxes.empty()) {
+			int crain_choose[51] = { 0, };
+			// 크레인 배정 시작
+			for (int i = 0; i < n; i++) {
+				for (int j = box_idx; j < boxes.size(); j++) {
+					if (boxes[j] <= crains[i]) {
+						boxes.erase(boxes.begin() + j);
+						box_move++;
+						break;
+					}
+				}
+			}
+			ans++;
+			if (box_move >= m) break;
+		}
+
+		cout << ans << '\n';
+	}
+
+	return 0;
+}
