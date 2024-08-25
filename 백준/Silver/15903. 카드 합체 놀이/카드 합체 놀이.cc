04578977c0c9b9b749ba7581388d96122ee1f8b1
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-08-25 22:56:42 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 124 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -0,0 +1,49 @@
+/*
+카드 합체 놀이
+n장의 카드
+1. x번 카드와 y번 카드를 골라 그 두 장에 쓰여진 수를 더한 값을 계산한다.
+2. 계산한 값을 x번 카드와 y번 카드 두 장에 모두 덮어쓴다.
+
+합체는 총 m번
+합체 점수를 가장 작게 만들지
+
+풀이 : 
+m < 15000
+O(2mlogm) = 약 12만 ? 충분할듯
+자료형 범위에 맞게 세팅하기
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+	cin >> n >> m;
+	vector<long long int> arr(n);
+
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	int cnt = 0;
+	while (cnt < m) {
+		sort(arr.begin(), arr.end());
+		long long int sum = arr[0] + arr[1];
+		arr.erase(arr.begin(), arr.begin() + 2);
+		arr.push_back(sum);
+		arr.push_back(sum);
+
+		cnt++;
+	}
+
+	long long int ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans += arr[i];
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
