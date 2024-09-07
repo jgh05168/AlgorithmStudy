14087951e05cdf227304b35b77fbc1f5f61b6297
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-09-07 11:18:06 +0900

    [Gold V] Title: 입국심사, Time: 864 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -1,27 +1,36 @@
 '''
-걸리는 시간에 대해서 이진탐색 수행
+k번 심사대에 앉아있는 심사관이 한 명을 심사하는 데 드는 시간은 Tk
 
-예상 시간(mid)를 각각 입국심사대로 나눠준 몫을 모두 더해서 모든 사람 수보다 큰 지 작은 지 판단
+풀이:
+친구들 <= 10^9 이기 때문에, 이분탐색 써야한다.
+-> 시간을 기준으로 풀이하기
+-> 최솟값을 찾기 위해
+    - start : 0
+    - end : 총 친구 수 x 가장 오래 걸리는 시간(최악)
 
+tmp += 시간 // 심사 시간
+
+if tmp < m : (mid + 1, end)
+else : (start, mid)
 '''
 
+import sys
+input = sys.stdin.readline
 
-N, M = map(int, input().split())
-T = [0] * N
-for i in range(N):
-    T[i] = int(input())
+n, m = map(int, input().split())
+t = [int(input()) for _ in range(n)]
 
-start, end = 0, max(T) * M
+start, end = 0, max(t) * m
 
-while start <= end:
+while start < end:
     mid = (start + end) // 2
-    temp = 0
-    for i in range(N):
-        temp += mid // T[i]
-
-    if temp < M:
+    # tmp 시간 계산해주기
+    tmp_time = 0
+    for tk in t:
+        tmp_time += mid // tk
+    if tmp_time < m:
         start = mid + 1
     else:
-        end = mid - 1
+        end = mid
 
 print(start)
