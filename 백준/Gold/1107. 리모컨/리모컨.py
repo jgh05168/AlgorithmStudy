8ab8791fce9c084ef55c81a4b8a539446af4eb04
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-09-24 21:54:28 +0900

    [Gold V] Title: 리모컨, Time: 2764 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -0,0 +1,32 @@
+'''
+리모컨
+
+채널은 무한대. 0밑으로 안내려간다
+풀이 :
+브루트포스
+그냥 깡으로 모든 경우 계산하기
+'''
+
+import sys
+input = sys.stdin.readline
+
+target = int(input())
+n = int(input())
+broken = list(map(int, input().split()))
+
+# 현재 채널에서 + 혹은 -만 사용하여 이동하는 경우
+min_count = abs(100 - target)
+
+for nums in range(1000001):
+    nums = str(nums)
+
+    for j in range(len(nums)):
+        # 각 숫자가 고장났는지 확인 후, 고장 났으면 break
+        if int(nums[j]) in broken:
+            break
+
+        # 고장난 숫자 없이 마지막 자리까지 왔다면 min_count 비교 후 업데이트
+        elif j == len(nums) - 1:
+            min_count = min(min_count, abs(int(nums) - target) + len(nums))
+
+print(min_count)
