1d9d00241d7f6980919b0d66facf30ddb2875bdd
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-09-27 11:25:25 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 328.25 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,11 +1,11 @@
 def solution(elements):
-    result = set()
-    
-    elementLen = len(elements)
-    # [7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
-    elements = elements * 2
-    
-    for i in range(elementLen):
-        for j in range(elementLen):
-            result.add(sum(elements[j:j+i+1]))
-    return len(result)
+    ll = len(elements)
+    res = set()
+
+    for i in range(ll):
+        ssum = elements[i]
+        res.add(ssum)
+        for j in range(i+1, i+ll):
+            ssum += elements[j%ll]
+            res.add(ssum)
+    return len(res)
