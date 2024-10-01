9c40be9a1154703c565ca332b1679a03d3282915
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-10-02 00:00:28 +0900

    [level 2] Title: 튜플, Time: 313.96 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -3,40 +3,27 @@
 1. 입력을 하나씩 순차적으로 받기
 2. 순서대로 튜플에 담기.
 '''
+'''
+풀이:
+1. 입력을 하나씩 순차적으로 받기
+2. 순서대로 튜플에 담기.
+'''
+
 def solution(s):
     answer = []
 
-    s = list(s.split(','))
-    s_list = []
-    tmp_list = []
-    tmp_s = ''
-    close = 1
-    for ss in s:
-        for c in ss:
-            if c == '{':
-                close = 0
-            elif c.isdigit() and not close:
-                tmp_s += c
-            elif c == '}':
-                if tmp_s != '':
-                    tmp_list.append(tmp_s)
-                    tmp_s = ''
-                if tmp_list:
-                    s_list.append(tmp_list)
-                    tmp_list = []
-                close = 1
-        if not close:
-            tmp_list.append(tmp_s)
-            tmp_s = ''
-    
-    s_list.sort(key=lambda x: len(x))
-    
-    comb_set = set()
-    for s in s_list:
-        for i in s:
-            if i not in comb_set:
-                comb_set.add(i)
-                answer.append(int(i))
-                break
+    s1 = s.lstrip('{').rstrip('}').split('},{')
+
+    new_s = []
+    for i in s1:
+        new_s.append(i.split(','))
+
+    new_s.sort(key = len)
+
+    for i in new_s:
+        for j in range(len(i)):
+            if int(i[j]) not in answer:
+                answer.append(int(i[j]))
 
     return answer
+
