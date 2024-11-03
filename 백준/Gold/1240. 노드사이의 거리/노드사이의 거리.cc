dfdb77004882326f2d7f316125b602870281a0f4
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-11-03 15:12:06 +0900

    [Gold V] Title: 노드사이의 거리, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,65 +1,81 @@
-/*
-N개의 노드로 이루어진 트리
-M개의 두 노드 쌍을 입력받을 때, 두 노드 사이의 거리 출력
-
-풀이:
-bfs, 입력 들어올 때마다 bfs 돌려서 찾기
-*/
-
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <cstring>
 #define INIT cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
 using namespace std;
 
-int n, m;
-int u, v, w;
-int visited[1001] = { 0, };
+const int MAX = 1001;
+const int LOG = 10; // 트리의 높이(log)를 결정할 수 있는 최대 값
+int depth[MAX];
+int parent[MAX][LOG];
+int dist[MAX];
+vector<pair<int, int>> graph[MAX];
 
+void dfs(int node, int par, int d) {
+	depth[node] = depth[par] + 1;
+	parent[node][0] = par;
+	dist[node] = d;
 
-int bfs(int su, int end, vector<vector<pair<int, int>>> graph) {
-	queue<pair<int, int>> q;
-	visited[su] = 1;
-	q.push({ su, 0 });
+	for (auto& edge : graph[node]) {
+		int next = edge.first;
+		int weight = edge.second;
+		if (next != par) {
+			dfs(next, node, d + weight);
+		}
+	}
+}
 
-	while (!q.empty()) {
-		u = q.front().first;
-		w = q.front().second;
-		q.pop();
+void setParent(int n) {
+	for (int j = 1; j < LOG; j++) {
+		for (int i = 1; i <= n; i++) {
+			parent[i][j] = parent[parent[i][j - 1]][j - 1];
+		}
+	}
+}
 
-		for (int i = 0; i < graph[u].size(); i++) {
-			v = graph[u][i].first;
-			int nw = graph[u][i].second;
-			if (!visited[v]) {
-				if (v == end)
-					return w + nw;
-				visited[v] = 1;
-				q.push({ v, w + nw });
-			}
+int lca(int u, int v) {
+	if (depth[u] < depth[v]) swap(u, v);
+
+	for (int i = LOG - 1; i >= 0; i--) {
+		if (depth[u] - (1 << i) >= depth[v]) {
+			u = parent[u][i];
 		}
 	}
 
-	return -1;
+	if (u == v) return u;
 
+	for (int i = LOG - 1; i >= 0; i--) {
+		if (parent[u][i] != parent[v][i]) {
+			u = parent[u][i];
+			v = parent[v][i];
+		}
+	}
+
+	return parent[u][0];
 }
 
 int main() {
+	INIT;
+	int n, m;
 	cin >> n >> m;
-	vector<vector<pair<int, int>>> graph(n + 1);
+
 	for (int i = 0; i < n - 1; i++) {
+		int u, v, w;
 		cin >> u >> v >> w;
 		graph[u].push_back({ v, w });
 		graph[v].push_back({ u, w });
 	}
 
-	int start, end;
-	while(m--) {
-		cin >> start >> end;
-		memset(visited, 0, 1001 * sizeof(int));
-		cout << bfs(start, end, graph) << '\n';
+	depth[0] = -1;
+	dfs(1, 0, 0); // 루트 노드를 1로 가정
+	setParent(n);
 
+	while (m--) {
+		int u, v;
+		cin >> u >> v;
+		int ancestor = lca(u, v);
+		cout << dist[u] + dist[v] - 2 * dist[ancestor] << '\n';
 	}
 
 	return 0;
