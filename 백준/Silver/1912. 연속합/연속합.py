5ebfd56969de2ee11301ef90527f1bd766e6ff4b
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-11-25 15:26:03 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -2,8 +2,9 @@ n = int(input())
 per = list(map(int, input().split()))
 
 dp = [0] * n
-dp[0] = per[0]
+dp[0] = per[0]      # 초기 값 저장
 for i in range(1, n):
-    dp[i] = max(per[i], per[i] + dp[i - 1])
-
+    # 이전까지 더했던 연속값의 합 + 현재값 & 현재값 의 최대값을 dp에 저장
+    dp[i] = max(per[i], per[i] + dp[i - 1])     
+    
 print(max(dp))
