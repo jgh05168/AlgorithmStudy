c977c9e793fb8aa58b842b16bc77303ac167ab10
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-11-25 16:13:09 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1000 ms, Memory: 105940 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,21 +1,21 @@
 '''
-for문 돌 때, 누적합 범위만큼 공식 세워서 더한 다음, 마지막 출력
+2차원 누적합
 
-겹치는 부분이 빠지게 된다.
-큰ㅁ - | - ㅡ + 겹치는ㅁ
+이건공식을 외웠지롱 : arr[x][y] = 원래값 + arr[x - 1][y] + arr[x][y - 1] - arr[x - 1][y - 1]
 '''
 
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+grid = [list(map(int, input().split())) for _ in range(n)]
 dp = [[0] * (n + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i - 1][j - 1]
+        dp[i][j] = grid[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
 
-for _ in range(m):
+for i in range(m):
     x1, y1, x2, y2 = map(int, input().split())
+    # 구간합 출력하기
     print(dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1])
