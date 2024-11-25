31157736d53048a4f6456917185331967df30845
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-11-25 15:04:40 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,14 +1,25 @@
-N, K = map(int, input().split())
-temp = [0] + list(map(int, input().split()))
-dp = [0] * (N + 1)
+'''
+연속적인 며칠 동안의 온도의 합이 가장 큰 지 알아보고자 함
 
-for i in range(1, N + 1):
-    if i < K:       # K만큼 더하지 못할 경우
-        dp[i] = dp[i - 1] + temp[i]     
-    else:           # K개씩 더할 수 있는 경우
-        dp[i] = dp[i - 1] - temp[i - K] + temp[i]   # 이전 경우에서 temp[i - K]번째를 뺀 뒤 현재 temp를 더해준다.
+풀이:
+완전탐색적으로 풀어나감
+for문 사용
+'''
 
-# K만큼 더하지 못하는 누적값들은 필요가 없으므로 pop
-for i in range(0, K):
-    dp.pop(0)   
-print(max(dp))
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+ans = 0
+for i in range(k):
+    ans += arr[i]
+
+# 시작
+tmp = ans
+for i in range(k, n):
+    tmp += -arr[i - k] + arr[i]
+    if ans < tmp:
+        ans = tmp
+
+print(ans)
