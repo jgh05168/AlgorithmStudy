77ae1bfa76a555ae0907fa302919bbe1361c2822
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-12-06 19:17:10 +0900

    [Gold V] Title: LCS, Time: 432 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,25 +1,22 @@
 '''
 LCS
 
-풀이 :
-1. 2중 for문 사용
-2. 만약 글자가 같다면 이전의 순열 + 1
-3. 글자가 다르다면 이전에 비교한 값들의 최댓값으로 업데이트
-    - ex) dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+모두의 부분 수열이 되는 수열 중 가장 긴 것 찾기
+고려해야 할 사항 : 한 개씩 비교해가며 맞나 아닌가를 판단해야함
+
+만약 현재 문자가 다른 문자와 같다면 ? dp[i - 1][j - 1] + 1 진행 => 현재 오고있던 값에 영향을 미치면 안된다.
+다르다면, dp[i - 1][j]와 dp[i][j - 1] 비교
 '''
 
-import sys
-input = sys.stdin.readline
+s1, s2 = input(), input()
+n, m = len(s1), len(s2)
 
-str1 = list(input().rstrip())   # 행
-str2 = list(input().rstrip())   # 열
-dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
-
-for i in range(1, len(str1) + 1):
-    for j in range(1, len(str2) + 1):
-        if str1[i - 1] == str2[j - 1]:        # 1빼줘야대
-            dp[i][j] = dp[i - 1][j - 1] + 1     # 이전까지 비교한 문자열의 위치에 대한 값이 필요하므로 행, 열 모두 -1 해줘야 한다.
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
 print(dp[-1][-1])
