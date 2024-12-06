8b40ce1b306ac46d14a7ede1d4d11e7cbd297ad0
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-12-06 18:33:23 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,24 +1,13 @@
-'''
-LIS : 가장 긴 증가하는 부분 수열
-
-- dp를 돌며 이전에 얼마나 증가하였는지에 대한 정보를 저장
-- 2중 for문을 사용하여 이전에 연결된 값과 현재 연결된 값 + 1에 대한 최대값을 저장한다.
-    - 만약 값이 큰 경우라면, <- 이 조건이 충족해야 함
-'''
-
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 arr = list(map(int, input().split()))
 
-# 자기 자신 역시 수열의 일부이므로 초기값을 모두 1로 설정
-dp = [1] * n
+# 생각해야 할 부분 : 증가하는 경우에 1씩 더해준다
 
-for i in range(1, n):
-    for j in range(i):
-        # 현재 보고 있는 값이 더 큰 경우에만 진행하기
-        if arr[i] > arr[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+dp = [0] * n
 
+for i in range(n):
+    dp[i] = max(dp[i], 1)
+    for j in range(i + 1, n):
+        if arr[i] < arr[j]:
+            dp[j] = max(dp[j], dp[i] + 1)
 print(max(dp))
