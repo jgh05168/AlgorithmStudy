fc9687e3c9703aa91aa8d98711b6245acb5e14f0
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-12-27 16:54:32 +0900

    [Gold II] Title: 컵라면, Time: 68 ms, Memory: 5252 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -8,6 +8,7 @@ n개의 문제를 풀었을 떄, 컵라면을 제공
 컵라면이 큰 값을 가져가되, 현재 데드라인에 임박한 녀석이 있다면, 먼저 가져가기
 -> 이걸 어떻게 알 것인가 ? 
 */
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -16,37 +17,41 @@ n개의 문제를 풀었을 떄, 컵라면을 제공
 using namespace std;
 
 int main() {
+	cin.tie(); cout.tie(); ios::sync_with_stdio(false);
 	int n;
 	cin >> n;
 
-	vector<pair<int, int>> problems(n);
+	vector<pair<int, int>> problems(n); // {데드라인, 컵라면 수}
 	for (int i = 0; i < n; i++) {
-		cin >> problems[i].first >> problems[i].second; // {데드라인, 컵라면 수}
+		cin >> problems[i].first >> problems[i].second;
 	}
 
-	// 1. 데드라인 기준 정렬
+	// 1. 데드라인 기준으로 오름차순 정렬
 	sort(problems.begin(), problems.end());
 
-	// 2. 최소 힙 사용
-	priority_queue<int, vector<int>, greater<int>> min_heap;
-	int total_noodles = 0;
+	// 2. 최소 힙(Min-Heap) 사용
+	priority_queue<int, vector<int>, greater<int>> pq; // 컵라면 수를 기준으로 최소값 관리
 
 	for (auto& problem : problems) {
 		int deadline = problem.first;
-		int noodles = problem.second;
+		int noodle = problem.second;
 
-		// 현재 문제를 힙에 추가
-		min_heap.push(noodles);
-		total_noodles += noodles;
+		// 힙에 추가
+		pq.push(noodle);
 
-		// 힙 크기가 데드라인을 초과하면 최소값 제거
-		if (min_heap.size() > deadline) {
-			total_noodles -= min_heap.top();
-			min_heap.pop();
+		// 힙 크기가 현재 데드라인 초과하면, 최소값 제거
+		if (pq.size() > deadline) {
+			pq.pop();
 		}
 	}
 
-	// 3. 결과 출력
-	cout << total_noodles << '\n';
+	// 3. 힙에 남아 있는 모든 값 더하기
+	int cup_noodles = 0;
+	while (!pq.empty()) {
+		cup_noodles += pq.top();
+		pq.pop();
+	}
+
+	cout << cup_noodles << '\n';
 	return 0;
 }
