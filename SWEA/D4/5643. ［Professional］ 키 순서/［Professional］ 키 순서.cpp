f9a9172f1759280eddd676af2322e8574b80716a
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-01-10 14:14:22 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,870 ms, Memory: 14,756 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.cpp
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.cpp
@@ -6,60 +6,70 @@
 using namespace std;
 
 int t, n, m;
-vector<int> to_tall[501];
-vector<int> to_small[501];
-int cnt[501];
+vector<vector<int>> graph(501);
+vector<vector<int>> rev_graph(501);
+int visited[501];
+int total_tall[501];   // 자신보다 키가 큰 학생 수
+int total_small[501];  // 자신보다 키가 작은 학생 수
 
-int bfs(const vector<int> graph[], int start) {
+void init() {
+    for (int i = 0; i <= n; i++) {
+        graph[i].clear();
+        rev_graph[i].clear();
+    }
+    memset(total_tall, 0, sizeof(total_tall));
+    memset(total_small, 0, sizeof(total_small));
+
+    cin >> n >> m;
+
+    int a, b;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        graph[a].push_back(b);     // 정방향 그래프
+        rev_graph[b].push_back(a); // 역방향 그래프
+    }
+}
+
+int bfs(int start, const vector<vector<int>> &graph) {
+    memset(visited, 0, sizeof(visited));
     queue<int> q;
-    bool visited[501] = {0}; // 방문 체크
-    visited[start] = true;
     q.push(start);
+    visited[start] = 1;
 
-    int reach = 0; // 도달 가능한 노드 수
+    int count = 0; // 도달 가능한 노드 수
     while (!q.empty()) {
         int u = q.front();
         q.pop();
+
         for (int v : graph[u]) {
             if (!visited[v]) {
-                visited[v] = true;
+                visited[v] = 1;
                 q.push(v);
-                reach++;
+                count++;
             }
         }
     }
-    return reach; // 자신 제외한 도달 노드 수
+    return count;
 }
 
 int main() {
     cin >> t;
     for (int tc = 1; tc <= t; tc++) {
-        // 그래프 초기화
-        for (int i = 0; i < 501; i++) {
-            to_tall[i].clear();
-            to_small[i].clear();
-        }
-        memset(cnt, 0, sizeof(cnt));
+        init();
 
-        cin >> n >> m;
-        int a, b;
-        for (int i = 0; i < m; i++) {
-            cin >> a >> b;
-            to_tall[a].push_back(b);
-            to_small[b].push_back(a);
-        }
-
-        // 각 학생별로 탐색
         for (int i = 1; i <= n; i++) {
-            cnt[i] += bfs(to_tall, i); // 자신보다 키가 큰 학생 수
-            cnt[i] += bfs(to_small, i); // 자신보다 키가 작은 학생 수
+            // BFS로 자신보다 키가 큰 학생 수 탐색
+            total_tall[i] = bfs(i, graph);
+            // BFS로 자신보다 키가 작은 학생 수 탐색
+            total_small[i] = bfs(i, rev_graph);
         }
 
-        // 결과 계산
         int ans = 0;
         for (int i = 1; i <= n; i++) {
-            if (cnt[i] == n - 1) // 자신 외의 모든 학생과 관계가 있으면
+            // 자신보다 키가 큰 학생 수 + 자신보다 키가 작은 학생 수 == n - 1
+            if (total_tall[i] + total_small[i] == n - 1) {
                 ans++;
+            }
         }
 
         cout << '#' << tc << ' ' << ans << '\n';
