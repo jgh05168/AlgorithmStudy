b43b380383567d0bdebdcb402c4685b944dd8762
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-01-20 12:50:17 +0900

    [level 3] Title: 불량 사용자, Time: 6.35 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 int n, m;
-int selected[8]; // user_id 선택 여부를 관리
-int dat[8];      // 제재 아이디 조합을 저장
+int selected[8];
+int dat[8];      // 제재 아이디 조합
 
 bool isMatch(const string& user, const string& banned) {
     if (user.size() != banned.size()) return false;
@@ -23,24 +23,24 @@ int recur(int depth, vector<string>& user_id, vector<string>& banned_id, vector<
         // 현재 조합 확인 및 중복 방지 처리
         vector<int> combination(dat, dat + m); // 현재 조합
         sort(combination.begin(), combination.end()); // 정렬
-        
+        cout << '\n';
         for (int i = 0; i < uniqueCombinations.size(); i++) {
             if (uniqueCombinations[i] == combination) {
                 return 0;
             }
         }
 
-        uniqueCombinations.push_back(combination); // 새로운 조합 추가
+        uniqueCombinations.push_back(combination);
         return 1;
     }
 
     int count = 0;
     for (int i = 0; i < n; i++) {
         if (!selected[i] && isMatch(user_id[i], banned_id[depth])) {
-            selected[i] = 1;     // 선택 표시
+            selected[i] = 1;     
             dat[depth] = i;      // 현재 depth에 해당하는 user_id의 인덱스를 저장
             count += recur(depth + 1, user_id, banned_id, uniqueCombinations);
-            selected[i] = 0;     // 선택 해제
+            selected[i] = 0;     
         }
     }
     return count;
@@ -52,6 +52,8 @@ int solution(vector<string> user_id, vector<string> banned_id) {
     memset(selected, 0, sizeof(selected));
     memset(dat, 0, sizeof(dat));
 
-    vector<vector<int>> uniqueCombinations; // 중복 방지용 컨테이너
-    return recur(0, user_id, banned_id, uniqueCombinations);
+    vector<vector<int>> uniqueCombinations;
+    int ans = recur(0, user_id, banned_id, uniqueCombinations);
+    
+    return ans;
 }
