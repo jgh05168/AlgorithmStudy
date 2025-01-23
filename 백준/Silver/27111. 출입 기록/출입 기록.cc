49855e9d6952840601daef9354e2f85cc33b6c91
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-01-23 22:11:46 +0900

    [Silver V] Title: 출입 기록, Time: 124 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/27111. 출입 기록/출입 기록.cc
@@ -0,0 +1,48 @@
+/*
+출입 기록의 일부가 누락됨
+n개 출입기록, 출입자가 출입한 시간순으로 기록됨
+누락된 출입 기록의 최소 개수를 구하자
+
+풀이 : dat
+
+0 or 1 체크해가면서 확인해가기
+- 0은 무조건 1일 때만 가능함
+- 1이 두 번 나오면 안됨
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int n, a, b;
+int arr[200001] = { 0, };
+
+int main() {
+
+	cin >> n;
+	int ans = 0;
+
+	for (int i=0;i<n;i++) {
+		cin >> a >> b;
+		if (b) {
+			if (arr[a])
+				ans++;
+			else
+				arr[a] = 1;
+		}
+		else {
+			if (arr[a])
+				arr[a] = 0;
+			else
+				ans++;
+		}
+	}
+
+	for (int i = 0; i < 200001; i++) {
+		if (arr[i])
+			ans++;
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
