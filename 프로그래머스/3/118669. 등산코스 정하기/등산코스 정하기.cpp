9e448e7773f3d9e9c3d26b25667a90456e3e8efc
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-01-24 00:21:41 +0900

    [level 3] Title: 등산코스 정하기, Time: 229.01 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
@@ -16,75 +16,75 @@ n개의 정점, 양방향 그래프
 #include <iostream>
 #include <algorithm>
 #include <queue>
-#include <unordered_map>
 
 #define INF 99999999
 
 using namespace std;
 
 
-int visited[50001] = {0, };
-unordered_map<int, int> summit_dict;
+int visited[50001] = { 0, };
+int dat[50001] = { 0, };
 
-void dijkstra(vector<int> &gates, vector<vector<pair<int, int>>> &graph, vector<int> &answer){
-    priority_queue<pair<int, int>> pq;
-    for (auto su : gates){
-        pq.push({0, su});
-        visited[su] = 0;
-    }
-    
-    while (!pq.empty()){
-        int w = pq.top().first;
-        int u = pq.top().second;
-        pq.pop();
-        
-        if (w > answer[1])
-            continue;
-        // cout << u << ' '<< w << '\n';
-        if (summit_dict[u]){
-            if (w < answer[1])
-                answer = {u, w};
-            else if (w == answer[1])
-                answer[0] = min(answer[0], u);
-            // cout << answer[0] << " ans " << answer[1] << '\n';
-            continue;
-        }
-        
-        for (auto v : graph[u]){
-            int new_cost = max(w, v.second);
-            if (visited[v.first] > new_cost){
-                pq.push({new_cost, v.first});
-                visited[v.first] = new_cost;
-                // cout << v.first << "   "<< visited[v.first] << '\n';
-            }
-        }
-    }
+
+void dijkstra(vector<int> &gates, vector<vector<pair<int, int>>> &graph, vector<int> &answer) {
+	priority_queue<pair<int, int>> pq;
+	for (auto su : gates) {
+		pq.push({ 0, su });
+		visited[su] = 0;
+	}
+
+	while (!pq.empty()) {
+		int w = pq.top().first;
+		int u = pq.top().second;
+		pq.pop();
+
+		if (w > answer[1])
+			continue;
+		// cout << u << ' '<< w << '\n';
+		if (dat[u]) {
+			if (w < answer[1])
+				answer = { u, w };
+			else if (w == answer[1])
+				answer[0] = min(answer[0], u);
+			// cout << answer[0] << " ans " << answer[1] << '\n';
+			continue;
+		}
+
+		for (auto v : graph[u]) {
+			int new_cost = max(w, v.second);
+			if (visited[v.first] > new_cost) {
+				pq.push({ new_cost, v.first });
+				visited[v.first] = new_cost;
+				// cout << v.first << "   "<< visited[v.first] << '\n';
+			}
+		}
+	}
 }
 
 vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
-    vector<int> answer;
-    answer.push_back(0);
-    answer.push_back(INF);
-    vector<vector<pair<int, int>>> graph(n + 1);
-    
-    // 그래프 생성
-    for (int i=0;i<paths.size();i++){
-        graph[paths[i][0]].push_back({paths[i][1], paths[i][2]});
-        graph[paths[i][1]].push_back({paths[i][0], paths[i][2]});
-    }
-    
-    // visited 초기화
-    for (int i=0;i<n + 1;i++){
-        visited[i] = INF;
-    }
-    
-    // 산봉우리 미리 표시
-    for (auto su : summits){
-        summit_dict[su] = 1;
-    }
-    
-    dijkstra(gates, graph, answer);
-    
-    
-    return answer;
+	vector<int> answer;
+	answer.push_back(0);
+	answer.push_back(INF);
+	vector<vector<pair<int, int>>> graph(n + 1);
+
+	// 그래프 생성
+	for (int i = 0; i < paths.size(); i++) {
+		graph[paths[i][0]].push_back({ paths[i][1], paths[i][2] });
+		graph[paths[i][1]].push_back({ paths[i][0], paths[i][2] });
+	}
+
+	// visited 초기화
+	for (int i = 0; i < n + 1; i++) {
+		visited[i] = INF;
+	}
+
+	// 산봉우리 미리 표시
+	for (auto su : summits) {
+		dat[su] = 1;
+	}
+
+	dijkstra(gates, graph, answer);
+
+
+	return answer;
 }
