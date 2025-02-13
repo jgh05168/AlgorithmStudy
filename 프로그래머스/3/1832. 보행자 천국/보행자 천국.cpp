fb09a9eb4395bd82f40fbf4f3a92a51d77ea84f8
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-02-13 12:57:30 +0900

    [level 3] Title: 보행자 천국, Time: 52.92 ms, Memory: 8.34 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
@@ -1,52 +1,78 @@
+/*
+도심의 일부 구역은 자동차 통행이 금지, 일부 교차로에서는 좌우회전 금지
+1: 자동차 통행 금지
+2: 직진만 가능
+도로 상태가 입력으로 주어졌을 때, 이동가능한 전체 가능 경로 수를 출력하자
+가능한 경로 수를 20170805로 나눈 나머지값을 출력
 
+풀이 : DFS + DP
+메모이제이션 방식을 사용한다.
+경로 이동은 동일하게 활용하기
+
+grid[r][c] 가 2인 경우는 따로 조건문 걸어주기
+*/
+
+
+#include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
 int MOD = 20170805;
-int M, N;
-int DY[] = { 0, 1 };
-int DX[] = { 1, 0 };
-vector<vector<vector<int>>> cache;
 
-int dfs(int y, int x, int dir, vector<vector<int>>& city_map)
-{
-  if (y + 1 == N && x + 1 == M)
-  {
-    return 1;
-  }
+int N, M;
+int grid[501][501] = { 0, };
+int dp[501][501][2];
 
-  int& ret = cache[y][x][dir];
-  if (ret != -1)
-  {
-    return ret;
-  }
+int dr[2] = { 0, 1 };
+int dc[2] = { 1, 0 };
 
-  ret = 0;
-  for (int i = 0; i < 2; i++)
-  {
-    int dy = y + DY[i];
-    int dx = x + DX[i];
-    if (dy < 0 || dy >= N || dx < 0 || dx >= M ||
-        city_map[dy][dx] == 1 ||
-        (city_map[y][x] == 2 && i != dir))
-    {
-      continue;
-    }
-    ret = (ret + dfs(dy, dx, i, city_map)) % MOD;
-  }
-  return ret;
+bool isValid(int r, int c) {
+	return 0 <= r && r < N && 0 <= c && c < M;
 }
 
+int dfs(int r, int c, int d) {
+	if (r == N - 1 && c == M - 1)
+		return 1;
+	if (grid[r][c] == 1)
+		return 0;
+	if (dp[r][c][d] != -1)
+		return dp[r][c][d];
+	int nr, nc;
+	int tmp = 0;
+	// 직진만 가능한 경우
+	if (grid[r][c] == 2) {
+		nr = r + dr[d], nc = c + dc[d];
+		if (isValid(nr, nc)) {
+			tmp += (dfs(nr, nc, d) % MOD);
+		}
+	}
+	else {
+		for (int nd = 0; nd < 2; nd++) {
+			nr = r + dr[nd], nc = c + dc[nd];
+			if (isValid(nr, nc)) {
+				tmp += (dfs(nr, nc, nd) % MOD);
+			}
+		}
+	}
+	dp[r][c][d] = tmp % MOD;
+	// cout << r << ' ' << c << ' ' << dp[r][c][d]<< '\n';
+	return dp[r][c][d];
+}
+
+
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
-int solution(int m, int n, vector<vector<int>> city_map)
-{
-  int answer = 0;
+int solution(int m, int n, vector<vector<int>> city_map) {
+	int answer = 0;
+	N = m, M = n;
+	memset(dp, -1, sizeof(dp));
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++)
+			grid[i][j] = city_map[i][j];
+	}
 
-  M = n;
-  N = m;
-  cache.assign(N, vector<vector<int>>(M, vector<int>(2, -1)));
-  answer = dfs(0, 0, 0, city_map);
+	answer = dfs(0, 0, 0);
 
-  return answer;
+	return answer;
 }
