d6e01105c8eb7079e6197d33899ff220d1d973ce
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-02-13 12:35:33 +0900

    [level 3] Title: 보행자 천국, Time: 21.08 ms, Memory: 7.58 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
@@ -1,78 +1,37 @@
-/*
-도심의 일부 구역은 자동차 통행이 금지, 일부 교차로에서는 좌우회전 금지
-1: 자동차 통행 금지
-2: 직진만 가능
-도로 상태가 입력으로 주어졌을 때, 이동가능한 전체 가능 경로 수를 출력하자
-가능한 경로 수를 20170805로 나눈 나머지값을 출력
-
-풀이 : DFS + DP
-메모이제이션 방식을 사용한다.
-경로 이동은 동일하게 활용하기
-
-grid[r][c] 가 2인 경우는 따로 조건문 걸어주기
-*/
-
-
-#include <iostream>
 #include <vector>
-#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
 int MOD = 20170805;
 
-int N, M;
-int grid[501][501] = { 0, };
-int dp[501][501][2];
-
-int dr[2] = { 0, 1 };
-int dc[2] = { 1, 0 };
-
-bool isValid(int r, int c) {
-	return 0 <= r && r < N && 0 <= c && c < M;
-}
-
-int dfs(int r, int c, int d) {
-	if (r == N - 1 && c == M - 1)
-		return 1;
-	if (grid[r][c] == 1)
-		return 0;
-	if (dp[r][c][d] != -1)
-		return dp[r][c][d];
-	int nr, nc;
-	int tmp = 0;
-	// 직진만 가능한 경우
-	if (grid[r][c] == 2) {
-		nr = r + dr[d], nc = c + dc[d];
-		if (isValid(nr, nc)) {
-			tmp += (dfs(nr, nc, d) % MOD);
-		}
-	}
-	else {
-		for (int nd = 0; nd < 2; nd++) {
-			nr = r + dr[nd], nc = c + dc[nd];
-			if (isValid(nr, nc)) {
-				tmp += (dfs(nr, nc, nd) % MOD);
-			}
-		}
-	}
-	dp[r][c][d] = tmp % MOD;
-	// cout << r << ' ' << c << ' ' << dp[r][c][d]<< '\n';
-	return dp[r][c][d];
-}
-
-
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int m, int n, vector<vector<int>> city_map) {
-	int answer = 0;
-	N = m, M = n;
-	memset(dp, -1, sizeof(dp));
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++)
-			grid[i][j] = city_map[i][j];
-	}
+    if ((n|m)==1) return 0;
 
-	answer = dfs(0, 0, 0);
+    int routes[m][n][2];
+    fill_n(&routes[0][0][0], m*n*2, 0);
 
-	return answer;
+    routes[0][0][0] = 1; // x
+    routes[0][0][1] = 1; // y
+    for (int x=0;x<m;x++) {
+        for (int y=0;y<n;y++) {
+            if ((x|y)==0) continue;
+
+            int* route = &routes[x][y][0];
+            int map = city_map[x][y];
+            if (map == 0) {
+                int cnt = 0;
+                if (x > 0) cnt = (cnt + routes[x-1][y][0]) % MOD;
+                if (y > 0) cnt = (cnt + routes[x][y-1][1]) % MOD;
+                route[0] = route[1] = cnt;
+            } else if (map == 1) {
+                route[0] = route[1] = 0;
+            } else if (map == 2) {
+                if (x > 0) route[0] = routes[x-1][y][0];
+                if (y > 0) route[1] = routes[x][y-1][1];
+            }
+        }
+    }
+    return routes[m-1][n-1][0];
 }
