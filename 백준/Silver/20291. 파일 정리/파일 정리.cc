e01f0e550edfabac9a58e0b7cd366df74d01931d
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-02-16 23:05:40 +0900

    [Silver III] Title: 파일 정리, Time: 88 ms, Memory: 11004 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.cc
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -1,11 +1,3 @@
-/*
-- 파일을 확장자 별로 정리해서 몇 개씩 있는지 알려줘
-- 보기 편하게 확장자들을 사전 순으로 정렬해줘
-
-풀이 : map
-
-*/
-
 #include <iostream>
 #include <string>
 #include <map>
@@ -18,20 +10,16 @@ string s;
 map<string, int> ans;
 
 int main() {
-	INIT
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> s;
-		int idx = s.find('.');
-		string key = "";
-		for (int j = idx + 1; j < s.length(); j++)
-			key += s[j];
-		ans[key]++;
-	}
+    INIT
+    cin >> n;
+    while (n--) {
+        cin >> s;
+        string ext = s.substr(s.find('.') + 1);  // 확장자 추출 최적화
+        ans[ext]++;
+    }
 
-	for (auto key : ans)
-		cout << key.first << ' ' << key.second << '\n';
+    for (const auto& key : ans)  // 불필요한 복사 방지
+        cout << key.first << ' ' << key.second << '\n';
 
-
-	return 0;
+    return 0;
 }
