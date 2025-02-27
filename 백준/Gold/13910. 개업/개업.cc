35e3b2ce2ff2e5b38e62cbfb93a1d150d38ebe4c
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2025-02-27 23:22:18 +0900

    [Gold V] Title: 개업, Time: 56 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/13910. 개업/개업.cc
@@ -0,0 +1,49 @@
+/*
+필요 이상 크기의 웍을 사용하지 않음. 한 번에 2개까지 사용 ㄱㄴ
+짜장면의 그릇 수에 딱 맞게 요리함.
+
+풀이 : 진하게 나는 dp향기 ..
+이번에도 topdown ?
+
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> vec;
+int dp[10100];
+
+int main() {
+	cin >> n >> m;
+
+	fill(dp, dp + 10100, 10000000);
+	dp[0] = 0;
+
+	int a;
+	for (int i = 0; i < m; i++) {
+		cin >> a;
+		vec.push_back(a);
+	}
+	vec.push_back(0);
+
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j < m; j++) {
+			for (int k = j + 1; k < m + 1; k++) {
+				int next = i + vec[j] + vec[k];
+				if (next <= n) {
+					dp[next] = min(dp[next], dp[i] + 1);
+				}
+			}
+		}
+	}
+
+	if (dp[n] >= 10000000) {
+		dp[n] = -1;
+	}
+	cout << dp[n];
+
+	return 0;
+}
